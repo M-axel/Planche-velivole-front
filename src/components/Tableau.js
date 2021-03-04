fa7e135420b20815aef574a10b8c5db3e90cbbb1
Axel <axel.maissa@etu.unice.fr>
2021-03-04 20:52:37 +0100

    Code HTML pour le tableau #4

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -3,7 +3,7 @@ import React from "react";
 import Ligne from "./Ligne";
 import "./Tableau.css";
 
-const Tableau = props => {
+const Tableau = (props) => {
   // Si le tableau n'a pas de ligne
   if (props.vol.length === 0) {
     return <h2>Il n'y a pas encore de vol aujourd'hui.</h2>;
@@ -11,6 +11,30 @@ const Tableau = props => {
 
   return (
     <table className="tableau">
+        <tbody>
+        <tr>
+          <th colSpan="3" scope="colgroup">AVION</th>
+          <th colSpan="3" scope="colgroup">PLANEUR</th>
+          <th colSpan="2" scope="colgroup">PILOTE PLANEUR</th>
+          <th colSpan="2" scope="colgroup">PASSAGER / INSTRUCTEUR</th>
+          <th colSpan="3" scope="colgroup">REMORQUAGE</th>
+          <th colSpan="2" scope="colgroup">PLANEUR</th>
+          <th colSpan="3" rowSpan="2" scope="colgroup">N°Parachute</th>
+        </tr>
+        <tr>
+          <th scope="col">Immat</th>
+          <th scope="col">Pilote</th>
+          <th scope="col">Code</th>
+          <th scope="col">Type</th>
+          <th colSpan = "2" scope="colgroup">Immat</th>
+          <th scope="col">Nom</th>
+          <th scope="col">Code</th>
+          <th scope="col">Nom</th>
+          <th scope="col">Code</th>
+          <th colSpan = "2" scope="colgroup">Décollage</th>
+          <th scope="col">Temps</th>
+          <th colSpan = "2" scope="colgroup">Atterrissage</th>
+        </tr>
       {/* On affiche pour toutes les lignes reçues par props (une ligne = un vol) */}
       {props.vol.map((vol) => (
         <Ligne
@@ -34,6 +58,7 @@ const Tableau = props => {
           numeroParachute={vol.parachute}
         />
       ))}
+      </tbody>
     </table>
   );
 };
