626a44f4f43ab023a1ddd2973ace588aca4f5db2
Axel <axel.maissa@etu.unice.fr>
2021-03-07 16:07:57 +0100

    Changement de méthode pour recuperer la date dans Archive. Qu'elle soit donnée en URL OU par un click sur calendrier => elle est placée en URL, le component se recharge et render alors la planche #11

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,9 +1,9 @@
-import React, { useState, useReducer } from "react";
-import { useParams } from "react-router-dom"; // Pour récupérer les éléments de l'url
+import React, { useReducer } from "react";
+import { useParams, useHistory, Link } from "react-router-dom"; // Pour récupérer les éléments de l'url
 
 import Calendrier from "../components/Archive/Calendrier";
 import Planche from "./Planche";
-import './Archive.css';
+import "./Archive.css";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -19,54 +19,68 @@ const reducer = (state, action) => {
 };
 
 const Archive = () => {
-// On regarde si il y a quelque chose dans l'url type "annee/mois/jour" au quel cas on l'affiche
-const dateULR = {
-  annee: useParams().annee,
-  mois: useParams().mois,
-  jour: useParams().jour,
-};
+  const history = useHistory();
+  // Il nous faut un état pour savoir si on est en selection de l'archive ou consultation
+  // Pour ça on va utiliser useReducer puisque les deux états sont liés
+  const [state, dispatch] = useReducer(reducer, "calendrier");
 
+  // On recupère ce qui se trouve dans l'url
+  const dateULR = {
+    annee: useParams().annee,
+    mois: useParams().mois,
+    jour: useParams().jour,
+  };
   // On converti ça en une date format ISO (ex: 2010-01-27)
   const dateURLISO = new Date(
     dateULR.annee + "-" + dateULR.mois + "-" + dateULR.jour
   );
 
-  // Il nous faut un état pour savoir si on est en selection de l'archive ou consultation
-  // Pour ça on va utiliser useReducer puisque les deux états sont liés
-  const [state, dispatch] = useReducer(reducer, "calendrier");
-
-  // useState définit la valeur de base (date de l'url OU aujourd'hui si date URL pas définit)
-  // Premier paramètre est l'état et le deuxième une fonction de MAJ
-  const [dateState, setDateState] = useState( dateURLISO || new Date());
-
-  //console.log(dateState);
+  console.log("Date:" + dateURLISO);
 
   const clickDayHandler = (date) => {
-    setDateState(date);
+    // Si une date est cliquée sur le calendrier, alors on modifie notre URL
+    // et au refresh, cette url sera capturée par useParams() et donc dans dateURL
+    history.push(
+      "/archive/" +
+        date.getFullYear() +
+        "/" +
+        date.getMonth() +
+        "/" +
+        date.getDay()
+    );
+
     dispatch({ type: "affichePlanche" });
     //console.log(state);
   };
-
 
   if (state === "calendrier") {
     return (
       <div className="Calendrier">
         {/*Le mecanisme de state se trouve ici, mais l'event est déclanché dans Calendar
           donc on passe à notre component de quoi rappeler */}
-        <Calendrier callback={clickDayHandler} date={dateState} />
+        <Calendrier callback={clickDayHandler} date={dateURLISO} />
       </div>
     );
   } else if (state === "planche") {
     return (
       <React.Fragment>
         <h1 className="titreArchive">
-          Archive du {dateState.getDay()}/{dateState.getMonth()}/{dateState.getFullYear()}
-          </h1>
-          <button className="retourCalendrier" onClick={() => dispatch({ type: "afficheCalendrier" })}>
-          &lt; Retour calendrier
+          Archive du {dateURLISO.getDay()}/{dateURLISO.getMonth()}/
+          {dateURLISO.getFullYear()}
+        </h1>
+        <div
+        className="retourCalendrier">
+        <Link
+          to="/archive"
+          onClick={() => dispatch({ type: "afficheCalendrier" })}
+        
+        ><button>
+        &lt; Retour calendrier
         </button>
+        </Link>
+        </div>
         {/** On passe la date en props à Planche */}
-          <Planche date={dateState} />
+        <Planche date={dateURLISO} />
       </React.Fragment>
     );
   }
